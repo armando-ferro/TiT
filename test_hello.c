7339d77f06419c938bdd66680d47785003f095de
jaimehrubiks <jaimehidalgo2@hotmail.com>
2017-02-07 18:33:41 +0100

    gets ya no es una función segura -Albus Dumbledore

--- test_hello.c
+++ test_hello.c
@@ -4,11 +4,17 @@
 
 #define MSG "Hello World"
 
+char * gets2(char *str){
+	fgets(str,256,stdin);
+	if(str[strlen(str)-1] == '\n') str[strlen(str)-1] = '\0';
+	return str;
+}
+
 int main()
 {
 int val=0;
 char buffer[256];
-	val=strcmp(gets(buffer),MSG);
+	val=strcmp(gets2(buffer),MSG);
 	printf("La cadena recibida es:<%s>\n",buffer);
 	printf("La cadena esperada es:<%s>\n",MSG);
 	return(val);
